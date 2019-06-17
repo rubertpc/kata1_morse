45288f618ac2db545445511f96a2702094f4697c
Rubert Pacheco <rubert.pacheco@gmail.com>
2019-06-18 00:31:31 +0200

    antes de creación de diccionario inverso

--- morse.py
+++ morse.py
@@ -45,6 +45,11 @@ morse = {
     '!': '——··——'
 }
 
+reverso = {}
+for key in morse:
+    valor = morse[key]
+    reverso[valor] = key
+
 def toMorse(texto):
     texto = texto.upper()
     resultado = ""
@@ -56,5 +61,9 @@ def toMorse(texto):
             resultado += " "
 
     return resultado
+
+def toPlain(codigo):
+    pass
+
             
         
